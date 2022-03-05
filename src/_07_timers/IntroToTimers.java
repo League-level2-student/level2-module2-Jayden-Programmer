48532cddd9058b5513fb782bf024889a8009ed7a
jayki <jayki@JayLaptop>
2022-03-05 13:36:43 -0800

    asdfasdfasdfasdf

--- src/_07_timers/IntroToTimers.java
+++ src/_07_timers/IntroToTimers.java
@@ -13,24 +13,24 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 /*
  * We're going to make a simple game using a timer. We'll have a countdown
  * in the middle of the screen, and the player needs to hit spacebar right 
  * as the countdown hits zero. 
  */
-public class IntroToTimers extends JPanel implements ActionListener, KeyListener{
-	static final int WIDTH = 500; 
+public class IntroToTimers extends JPanel implements ActionListener, KeyListener {
+	static final int WIDTH = 500;
 	static final int HEIGHT = 500;
 	JFrame frame = new JFrame();
 	Font font = new Font("Arial", Font.BOLD, 50);
 	int time;
 	int maxTime = 45;
 	int speed = 30;
-	
+
 	// 1. Declare a Timer variable, but do not initialize it.
 	Timer timer;
-	private Object newTimer; 
-	 
+	private Object newTimer;
 
 	public static void main(String[] args) {
 		IntroToTimers tg = new IntroToTimers();
@@ -41,67 +41,61 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 		frame.add(this);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension (WIDTH,HEIGHT));
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		frame.pack();
 		frame.addKeyListener(this);
-		
-		
-		
-		// 2. Set the time variable to maxTime
-	timer.equals(maxTime);
-		// 3. Initialize timer to a new Timer object. For the first argument, use 1000/speed. For the second argument, use this
-		timer.equals(newTimer);
-		newTimer.equals(1000);
-		// 11. Run it! Notice how it doesn't update? That's because our timer isn't triggering the actionPerformed() method yet!
-		// 12. Make the timer start!
 
-		// 13. Run the program. Is it possible for you to win? 
-		// 14. Figure out how to make it harder or easier! (Which condition is most related to whether you win or not?)
+		// 2. Set the time variable to maxTime
+		time = maxTime;
+		// 3. Initialize timer to a new Timer object. For the first argument, use
+		// 1000/speed. For the second argument, use this
+		timer = new Timer(1000 / speed, this);
+
+		// 11. Run it! Notice how it doesn't update? That's because our timer isn't
+		// triggering the actionPerformed() method yet!
+
+		// 12. Make the timer start!
+		timer.start();
+		// 13. Run the program. Is it possible for you to win?
+
+		// 14. Figure out how to make it harder or easier! (Which condition is most
+		// related to whether you win or not?)
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// 8. Decrement time
+		time--;
 
 		// 9. if time is less than or equal to 2...
+		if (time <= 2) {
+			time = maxTime;
+		}
+		// 10. Set time equal to maxTime
 
-			// 10. Set time equal to maxTime
-
-		
 		repaint();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-		
-		// 4. if keyCode is 32 (that's the spacebar key!)...
-		if (keyCode = e.getSource("32")) { 
-			
-		}
 
+		// 4. if keyCode is 32 (that's the spacebar key!)...
+		if (keyCode == 32) {
 
 			// 5. if time is less than 2...
 			if (time < 2) {
-			JOptionPane.showMessageDialog(null, "You Win!");	
-			} else if (time == maxTime) {
-				
+				JOptionPane.showMessageDialog(null, "You Win!");
+			} else {
+			time = maxTime; 
 			}
-			}
-				
-		
-			 
+		}
+	}
 
-			
-			
-	
-				// 6. Tell them they win!
-		
-			
-			// 7. Otherwise, set time equal to maxTime
-			
-		
-	
+	// 6. Tell them they win!
+
+	// 7. Otherwise, set time equal to maxTime
 
 	@Override
 	public void paintComponent(Graphics g) {
@@ -109,9 +103,9 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(Color.RED);
 		g.setFont(font);
-		g.drawString(time+"", WIDTH/2-50, HEIGHT/2);
+		g.drawString(time + "", WIDTH / 2 - 50, HEIGHT / 2);
 	}
-	
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 	}
